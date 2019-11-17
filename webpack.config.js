2736ad7b28ab67df5906330a6613db690f578cb5
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-17 08:55:03 -0400

    added loaders for .js and .scss files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ const webpack = require("webpack")
 
 
 module.exports = {
-    entry: "/app/src/client/index.js",
-    rules: [],
+    entry: "./app/src/client/index.js",
+    rules: [
+        {
+            test:/\(.js|.jsx)/,
+            loader: babel-loader
+        },
+        {
+            test:/\.scss/,
+            use:[style-loader, css-loader, sass-loader]
+        }
+    ],
     plugins: []
 }
