b3442142e364378d899e0136e26026c905cc6f3f
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-17 08:22:58 -0400

    initial file setup

---
+++ app/src/client/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export class App extends Component {
+
+    render() {
+        return (
+            <div>
+                Hi!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render()
