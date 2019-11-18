b41e6d19ca78832a96f678d129dfac030a3f5f16
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-18 16:40:49 -0400

    Battle component setup

---
+++ app/src/components/Battle.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Instructions() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export class Battle extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Instructions />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Battle
