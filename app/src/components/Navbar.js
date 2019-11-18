ec2563d90ffaa13a5a9a922845f551f7b98384fa
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-17 20:55:22 -0400

    Navbar function component extracted

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,21 +1,49 @@
 import React from 'react'
 
-function Navbar() {
+function NavContent ({lang, callback}){
     const navbarItems = ["All", "Javascript", "Ruby", "Python", "Java", "CSS"]
     return (
-        <React.Fragment>
+        <div>
             <h1>Navigation</h1>
             <ul id="navList">
             {navbarItems.map((item)=>(
                 <li className="navItem" key={item}>
-                    <button className="clearButton navLink">
+                    <button 
+                    className="clearButton navLink"
+                    onClick={()=>callback(item)}
+                    style={item==={lang} ? {color: "purple"} : {color: "black"}}> 
                         {item}
                     </button>
                 </li>
             ))} 
             </ul>
-        </React.Fragment>
+        </div>
     )
+}
+
+class Navbar extends React.Component{
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            language: "All", 
+        }
+        this.navSelector=this.navSelector.bind(this);
+    }
+    
+     navSelector(lang){
+        this.setState({language: lang},()=>{console.log(this.state.language)})
+    }
+
+    render(){
+        return (
+        <NavContent 
+        lang={this.state.language} 
+        callback={this.navSelector} />
+        )
+    }
+
 }
 
 export default Navbar
