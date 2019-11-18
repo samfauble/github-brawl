e82d70017b05a962a64bbef60c50e0b55698454c
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-18 16:02:36 -0400

    Repo list without styling or icons

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -28,6 +28,33 @@ NavContent.propTypes = {
     callback: PropTypes.func.isRequired
 }
 
+function ReposGrid({repos}) {
+    return(
+    <ul className="grid spaceAround">
+        {repos.map((repo, index)=>{
+        const {name, owner, html_url, stargazers_count, forks, open_issues} = repo
+        const {login, avatar_url} = owner
+        
+        return (
+            <li key={html_url} className="repo bg-light">
+                <h4 className="header-lg centerText">
+                    `#{index+1}`
+                </h4>
+                <img 
+                    className="avatar"
+                    src={avatar_url}
+                    alt={`Avatar for ${login}`}/>
+                <h2 className="centerText">
+                    <a className="link" href={html_url}> {login} </a>
+                </h2>
+            </li>
+        )
+            
+        })}
+    </ul>
+    )
+}
+
 class Navbar extends React.Component{
     
     constructor(props) {
@@ -35,41 +62,53 @@ class Navbar extends React.Component{
     
         this.state = {
             language: "All",
-            repos: null,
+            repos: {},
             error: null 
         }
         this.navSelector=this.navSelector.bind(this);
         this.isLoading = this.isLoading.bind(this)
     }
+
     
      navSelector(lang){                                                 {/*Given the chosen language, set the new language, refresh errors and repos, and fetch repos for given language*/}
-        this.setState({language: lang, repos: null, error: null },
+        this.setState({language: lang, error: null },
             ()=>{console.log(this.state.language)})
-        
-        fetchRepos(lang).then((repos)=>this.setState({
-            repos,
-            error: null
-        }))
-        .catch(()=>{
-            console.warn("Error fetching repos", error)
 
-            this.setState({error:"There was an error in fetching the repositoriess"})
+        if(!this.state.repos[lang]){
+            fetchRepos(lang).then((data)=>{this.setState(({repos})=>({
+                repos: {
+                    ...repos,
+                    [lang]: data
+                }
+            }))
         })
+            .catch(()=>{
+                console.warn("Error fetching repos", error)
+    
+                this.setState({error:"There was an error in fetching the repositoriess"})
+            })
+        }
     }
 
+    componentDidMount(){
+        this.navSelector(this.state.language)
+    }
 
     isLoading(){
-        return this.state.repos === null && this.state.error===null
+            const {language, repos, error}=this.state
+            return !repos[language] && error===null 
     }
 
     render(){
-        const [lang, repos, error]=this.state
+        const {language, repos, error}=this.state
         return (
         <React.Fragment>
             <NavContent 
             lang={this.state.language} 
             callback={this.navSelector} />
             {this.isLoading() && <p>LOADING</p>}
+            {error && <p>error</p>}
+            {repos[language] && <ReposGrid repos={repos[language]} />}
         </React.Fragment>
         )
     }
