704b62415eecb3384157e125ff4f0133384431c1
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-18 20:35:04 -0400

    Input field with styling added to Battle

--- app/src/client/index.js
+++ app/src/client/index.js
@@ -2,13 +2,14 @@ import "./style/index.scss"
 import React from "react"
 import ReactDOM from "react-dom"
 import Navbar from "../components/Navbar"
+import Battle from "../components/Battle"
 
 
 class App extends React.Component {
     render() {
         return (
             <div className="container">
-               <Navbar /> 
+               <Battle /> 
             </div>
         )
     }
--- app/src/components/Battle.js
+++ app/src/components/Battle.js
@@ -1,18 +1,99 @@
 import React from 'react'
+import {FaUserFriends, FaFighterJet, FaTrophy} from "react-icons/fa"
 
 function Instructions() {
     return(
-        <div>
-            
+        <div className="instructionsContainer">
+            <h1 className= "centerText header-lg">
+            INSTRUCTIONS
+            </h1>
+            <ol className="container-sm grid centerText battleInstructions">
+                <li>
+                    <h3 className="header-sm">Enter two GitHub users</h3>
+                    <FaUserFriends className="bg-light" color="blue" size={140}/>
+                </li>
+                <li>
+                    <h3 className="header-sm">Brawl</h3>
+                    <FaFighterJet className="bg-light" color="blue" size={140}/>
+                </li>
+                <li>
+                    <h3 className="header-sm">See the winners</h3>
+                    <FaTrophy className="bg-light" color="blue" size={140}/>
+                </li>
+            </ol>
         </div>
     )
 }
 
-export class Battle extends React.Component {
+class Players extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             username: ""
+        }
+        this.playerChange = this.playerChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.username)
+    }
+
+    playerChange(e){
+        this.setState({
+            username: e.target.value
+        })
+    }
+    
     render() {
+        return (
+            <form className="column player" onSubmit={this.handleSubmit}>
+                <label htmlFor="username" className="playerLabel">
+                    {this.props.label}
+                </label>
+                <div className="row playerInputs">
+                    <input
+                        id="username"
+                        className="inputLight"
+                        placeholder="github username"
+                        autoComplete="off"
+                        value={this.state.username}
+                        onChange={this.playerChange}  
+                        type="text" />
+                    <button 
+                        type="submit" 
+                        className="button buttonDark" 
+                        disabled={!this.state.username}>
+                        Submit
+                </button>
+                </div>
+            </form>
+        )
+    }
+}
+
+export class Battle extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             playerOne: null,
+             playerTwo: null
+        }
+    }
+    
+
+    render() {
+      
         return (
             <React.Fragment>
                 <Instructions />
+                <Players 
+                label="Player" 
+                onSubmit={(value)=>{console.log(value)}} />
             </React.Fragment>
         )
     }
