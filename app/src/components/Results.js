92f3ac2ecb33a47dcc15885146b299222b6eb047
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-18 22:33:43 -0400

    Created api for the brawl function

--- app/src/components/Results.js
+++ app/src/components/Results.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import {brawl} from "../util/api"
 
 export class Results extends React.Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             winner: null,
+             loser: null,
+             error: null,
+             loading: true
+        }
+    }
+    
+
+    componentDidMount() {
+        const {playerOne, playerTwo} = this.props
+        brawl([playerOne, playerTwo])
+        .then((players)=> {
+            this.setState({
+                winner: players[0],
+                loser: players[1],
+                error: null,
+                loading: false
+            })
+        }).catch(({message}) => {
+            this.setState({
+                error: message,
+                loading: false
+            })
+        })
+    }
+
     render() {
         return (
             <div>
                 Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
+                <pre>{JSON.stringify(this.state, null, 2)}</pre>
             </div>
         )
     }
